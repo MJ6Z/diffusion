a33aeb6fd3af0ee056517116381495f9476b1ea1
MJ6Z <110768811+MJ6Z@users.noreply.github.com>
2023-12-06 09:56:29 +0000

    initial commit of code modified for diffusion

---
+++ calc.h
@@ -0,0 +1,136 @@
+#include <vector>
+#include <array>
+#include <sstream>
+#include <morph/RD_Base.h>
+#include <morph/HdfData.h>
+
+/*!
+ * Diffusion system.
+ */
+
+/*Inherit methods and attributes from the Reaction diffusion class (RD_base) for laplacian computation*/
+template <typename Flt>
+class D_calc : public morph::RD_Base<Flt>{
+public:
+
+
+    alignas(alignof(std::vector<Flt>))
+    std::vector<Flt> phi;
+
+    /*
+     * The diffusion parameters.
+     */
+    alignas(Flt) Flt D_phi = 0.1;
+
+    /*Runge-Kutta params*/
+    alignas(Flt) Flt k1 = 1.0;
+    alignas(Flt) Flt k2 = 1.0;
+    alignas(Flt) Flt k3 = 1.0;
+    alignas(Flt) Flt k4 = 1.0;
+
+    /*!
+     * Constructor
+     */
+    D_calc() : morph::RD_Base<Flt>() {}
+    /*!
+     * Destructor
+     */
+    ~D_calc() {} // No operation. RD_Base destructor will free HexGrid.
+
+    void allocate()
+    {
+        // Always call allocate() from the base class first.
+        morph::RD_Base<Flt>::allocate();
+        // Resize and zero-initialise the various containers. Note that the size of a
+        // 'vector variable' is given by the number of hexes in the hex grid which is
+        // a member of this class (via its parent, RD_Base)
+        this->resize_vector_variable (this->phi);
+    }
+
+    void init()
+    {
+        // Initialise Phi with noise
+        this->noiseify_vector_variable (this->phi, 0.5, 1);
+    }
+
+
+/*computing dPhi/dt = phi * D * laplacian*/
+    void compute_dPhidt (std::vector<Flt>& phi_, std::vector<Flt>& dPhidt)
+    {
+        /*creating a vector for the laplacian over the hexgrid 'nhex'*/
+        std::vector<Flt> lapPhi(this->nhex, 0.0);
+        /*computing the laplacian, mapping compute_laplace(phi(x,y))=>lapPhi(x,y)*/
+        this->compute_laplace (phi_, lapPhi);
+#pragma omp parallel for /*spawns a group of threads and divides loop iterations between them*/
+/*computes dPhi/dt for all hexes*/
+        for (unsigned int h=0; h<this->nhex; ++h) {
+            dPhidt[h] =  this->D_phi * lapPhi[h] * phi_[h];
+        }
+    }
+
+
+
+
+
+void step()
+    {
+        this->stepCount++;
+
+        // 1. 4th order Runge-Kutta computation for Phi
+        {
+            // Phitst: "Phi at a test point". Phitst is a temporary estimate for Phi.
+            std::vector<Flt> Phitst(this->nhex, 0.0);
+            std::vector<Flt> dPhidt(this->nhex, 0.0);
+            std::vector<Flt> K1(this->nhex, 0.0);
+            std::vector<Flt> K2(this->nhex, 0.0);
+            std::vector<Flt> K3(this->nhex, 0.0);
+            std::vector<Flt> K4(this->nhex, 0.0);
+            /*
+             * Stage 1
+             */
+            this->compute_dPhidt (this->phi, dPhidt);
+#pragma omp parallel for
+            for (unsigned int h=0; h<this->nhex; ++h) {
+                K1[h] = dPhidt[h] * this->dt;
+                Phitst[h] = this->phi[h] + K1[h] * 0.5 ;
+            }
+
+            /*
+             * Stage 2
+             */
+            this->compute_dPhidt (Phitst, dPhidt);
+#pragma omp parallel for
+            for (unsigned int h=0; h<this->nhex; ++h) {
+                K2[h] = dPhidt[h] * this->dt;
+                Phitst[h] = this->phi[h] + K2[h] * 0.5;
+            }
+
+            /*
+             * Stage 3
+             */
+            this->compute_dPhidt (Phitst, dPhidt);
+#pragma omp parallel for
+            for (unsigned int h=0; h<this->nhex; ++h) {
+                K3[h] = dPhidt[h] * this->dt;
+                Phitst[h] = this->phi[h] + K3[h];
+            }
+
+            /*
+             * Stage 4
+             */
+            this->compute_dPhidt (Phitst, dPhidt);
+#pragma omp parallel for
+            for (unsigned int h=0; h<this->nhex; ++h) {
+                K4[h] = dPhidt[h] * this->dt;
+            }
+
+            /*
+             * Final sum. This could be incorporated in the for loop for stage 4.
+             */
+#pragma omp parallel for
+            for (unsigned int h=0; h<this->nhex; ++h) {
+                phi[h] += ((K1[h] + 2.0 * (K2[h] + K3[h]) + K4[h])/(Flt)6.0);
+            }
+        }
+    }
+};
---
+++ hexvis.cpp
@@ -0,0 +1,212 @@
+
+#ifndef FLT
+# error "Please define FLT when compiling (hint: See CMakeLists.txt)"
+#endif
+
+
+/*!
+ * Include the calculation class.
+ */
+#include "calc.h"
+
+/*!
+ * Standard library includes here
+ */
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <chrono>
+/*
+ * Morphologica includes here
+ */
+# include <morph/Visual.h>
+# include <morph/HexGridVisual.h>
+# include <morph/ColourMap.h>
+# include <morph/VisualDataModel.h>
+# include <morph/Scale.h>
+# include <morph/vec.h>
+/*
+ * Other useful morph includes.
+ */
+#include <morph/tools.h>
+#include <morph/Config.h> //json read-writer
+
+/*
+main() is in crontrol of the simulation. Parameters are controlled via JSON,
+argc and argv are used to pass in params.json at execution.*/
+
+
+/*Lots of explict type definition is used in this code. e.g. 100UL and 0.3f*/
+
+int main(int argc, char **argv){
+    
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " /path/to/params.json" << std::endl;
+        return 1;
+    }
+    std::string paramsfile (argv[1]);
+
+    /*
+     * Set up morph::Config JSON reader/writer for reading the parameters
+     * Return error if this fails.
+     */
+    morph::Config conf(paramsfile);
+    if (!conf.ready) {
+        return 1;
+    }
+    
+
+    //getting simulation-wide parameters from JSON
+    // usage here: conf.get("find a value", if not found use this value:)
+    const unsigned int steps = conf.getUInt ("steps", 1000UL);
+    if (steps == 0) {
+        std::cerr << "Not much point simulating 0 steps! Exiting." << std::endl;
+        return 1;}
+
+    const FLT dt = static_cast<FLT>(conf.getDouble("dt", 0.00001));
+
+    //returning No of steps.
+    std::cout << "steps to simulate: " << steps << std::endl;
+
+/*
+ *
+ * Visualisation stuff.
+ *
+ */
+
+// Parameters for plotting:
+const unsigned int plotevery = conf.getUInt ("plotevery", 10);
+
+// Window width and height
+const unsigned int win_width = conf.getUInt ("win_width", 1025UL);
+unsigned int win_height_default = static_cast<unsigned int>(0.8824f * (float)win_width);
+const unsigned int win_height = conf.getUInt ("win_height", win_height_default);
+
+// Set up the morph::Visual object:
+morph::Visual v1 (win_width, win_height, "Hex Diffusion");
+
+// Set a dark blue background, RGB:
+v1.bgcolour = {conf.getFloat("bgR", 0.2f), conf.getFloat("bgG", 0.2f), conf.getFloat("bgB", 0.2f), 1.0f};
+// setting clipping planes & FOV.
+v1.zNear = 0.001;
+v1.zFar = 10000;
+v1.fov = 45;
+
+// unlock the scene (you cna pan around the visual)
+v1.sceneLocked = conf.getBool ("sceneLocked", false);
+
+// You can set the scene x/y/z offsets.
+v1.setZDefault (conf.getFloat ("z_default", 0.0f));
+v1.setSceneTransXY (conf.getFloat ("x_default", 0.0f),
+                    conf.getFloat ("y_default", 0.0f));
+
+// scroll in-out speed.
+v1.scenetrans_stepsize = 0.5;
+
+// setting up the render clock.
+std::chrono::steady_clock::time_point lastrender = std::chrono::steady_clock::now();
+
+
+
+/*
+*
+*
+* Instantiating & setting up the model object class found in calc.h
+*
+*
+*/
+    D_calc<FLT> D;
+
+    D.svgpath = conf.getString ("svgpath", "");
+    D.ellipse_a = conf.getDouble ("ellipse_a", 0.6);
+
+    // Control the size of the hexes, and therefore the number of hexes in the grid
+    D.hextohex_d = conf.getFloat ("hextohex_d", 0.01f);
+    D.hexspan = conf.getFloat ("hexspan", 4.0f);
+    //hexspan/hextohex = No of hexes.
+
+    // Boundary fall-off distance, this is will ensure the system rolls off to zero at the boundary.
+    //This is essentially neutrons being absorbed by the casing of the reactor.
+    D.boundaryFalloffDist = conf.getFloat ("boundaryFalloffDist", 0.1f);
+
+    // After setting the first few features, we can call the allocate function to set
+    D.allocate();
+
+    // After allocate(), we can set up parameters:
+    D.set_dt(dt);
+    D.k1 = conf.getDouble ("k1", 1);
+    D.k2 = conf.getDouble ("k2", 1);
+    D.k3 = conf.getDouble ("k3", 1);
+    D.k4 = conf.getDouble ("k4", 1);
+    D.D_phi = conf.getDouble ("D_phi", 0.1);
+
+    // Now parameters are set, call init(), which in this example simply initializes Phi with some noise.
+    D.init();
+
+/*
+* This is the end of model setup.
+*/
+
+
+
+// Spatial offset, for positioning of HexGridVisuals
+morph::vec<float> spatOff;
+float xzero = 0.0f;
+
+xzero -= 0.5 * D.hg->width();
+spatOff = { 0.0, 0.0, 0.0 };
+
+
+morph::ColourMapType cmt = morph::ColourMap<FLT>::strToColourMapType (conf.getString ("colourmap", "Jet"));
+
+// Create a new HexGridVisual hgv1 then set its parameters:
+auto hgv1 = std::make_unique<morph::HexGridVisual<FLT>> (D.hg, spatOff);
+v1.bindmodel (hgv1);
+hgv1->setScalarData (&D.phi);
+// Z position scaling - how hilly/bumpy the visual will be.
+hgv1->zScale.setParams (0.4f, 0.0f);
+// The second is the colour scaling. Set this to autoscale.
+hgv1->colourScale.do_autoscale = true;
+hgv1->cm.setType (cmt);
+
+// "finalize" is required before adding the HexGridVisual to the morph::Visual.
+hgv1->finalize();
+auto hgv1p = v1.addVisualModel (hgv1);
+
+
+    // Start the loop
+    bool finished = false;
+    while (finished == false) {
+        // Step the model
+        D.step();
+
+        if ((D.stepCount % plotevery) == 0) {
+            // These two lines update the data for the two hex grids. That leads to
+            // the CPU recomputing the OpenGL vertices for the visualizations.
+            hgv1p->updateData (&(D.phi));
+            hgv1p->clearAutoscaleColour();
+        }
+        // rendering the graphics. After each simulation step, check if enough time
+        // has elapsed for it to be necessary to call v1.render().
+        std::chrono::steady_clock::duration sincerender = std::chrono::steady_clock::now() - lastrender;
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(sincerender).count() > 17) { // 17 is about 60 Hz
+            glfwPollEvents();
+            v1.render();
+            lastrender = std::chrono::steady_clock::now();
+        }
+
+        if (D.stepCount > steps) {
+            finished = true;
+        }
+    }
+
+
+    std::cout << "Ctrl-c or press x in graphics window to exit.\n";
+    v1.keepOpen();
+
+return 0;
+}
